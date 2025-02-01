4a5d637cd521338f70b6d2754ab02567c71effad
palkikhare21 <palkikhare2@gmail.com>
2025-02-01 08:14:12 +0530

    FSD

--- javascript/api7.js
+++ javascript/api7.js
@@ -1,3 +1,4 @@
+ 
 fetch('https://dummyjson.com/recipes').then((res)=>{
     return res.json()
 }).then((data)=>{
@@ -8,16 +9,19 @@ fetch('https://dummyjson.com/recipes').then((res)=>{
 let div= document.querySelector('div')
 function call(data){
     console.log(data,"hahah");
-    let h1= document.querySelector('img')
+    // let h1= document.querySelector('img')
 
     for(let i of data){
-        let h1= document.createElement('h1')
-        
+        let img = document.createElement('img')
+        img.setAttribute('src',i.image);
+        img.style.width="280px";
+        img.style.margin="10px";
+
 
         // for(let i of data)
-        h1.setAttribute('src',i.image)     
-          // h1.innerText=i.name
-        // div.append(h1)
+        // h1.setAttribute('src',i.image)     
+        //   h1.innerText=i.name
+        div.append(img);
     }
     
 }
