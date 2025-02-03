22df9d9ab03ec0698b654aed519912934f5dd558
palkikhare21 <palkikhare2@gmail.com>
2025-02-03 09:57:06 +0530

    FSD

--- javascript/index8.js
+++ javascript/index8.js
@@ -4,7 +4,7 @@ let pen = canvas.getContext('2d')
 let cell=50
 let snakeCells=[[0,0]]
 let direction='right'
-
+let gameOver=false 
 
 document.addEventListener('keydown',(e)=>{
     if(e.key==='Arrowup'){
@@ -26,6 +26,13 @@ setInterval(()=>{
 },200)
 
 function shapeCreate(){
+    if(gameOver){
+        clearInterval(id)
+        pen.fillStyle='red';
+        pen.font='40px sans-serif';
+        pen.fillText('Game Overi',50,150);
+        return
+    }
     
         pen.clearRect(0,0,1000,600)
         for(let i of snakeCells){
@@ -50,20 +57,33 @@ let newY
 if(direction==='right'){ 
     newX=headX+cell,
     newY=headY
+
+    if(newX===1000){
+        gameOver=true
+    }
    }
    else if(direction=='left'){
     newX=headX-cell
     newY=headY
+    if(newX<0){
+        gameOver=true
+    }
 
    }
    else if(direction=='up'){
     newX=headX
     newY=headY-cell
+    if(newY<0){
+        gameOver=true
+    }
    
    }
    else{
     newX=headX
     newY=headY+cell
+    if(newY<0){
+        gameOver=true
+    }
  
    }
    snakeCells.push([newX,newY])
