8164d601e12944ea54c4eae65b3eb45332c21466
palkikhare21 <palkikhare2@gmail.com>
2025-02-05 07:46:47 +0530

    FSD

---
+++ javascript/index30.js
@@ -0,0 +1,26 @@
+// let arr=[1,2,3,4,5]
+// let num=[...arr,6,7,8,9]
+
+// console.log(num);
+
+// function sum(a,b,c,...rest){
+//     console.log(resizeTo,"hiiiee");
+    
+// }
+// sum(1,2,3,4,5,6,7,8)
+
+let obj={
+    id:1,
+    name:'hello'
+}
+// obj.name 
+let{id,name}=obj
+console.log(name);
+
+let arr=[1,2,3,4,5,6,7,8]
+
+// console.log(arr[3]);
+
+let [a,b]=arr
+console.log(a);
+
