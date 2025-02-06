3e6735e02990a4113381b5b15b3246be7c76bfd5
palkikhare21 <palkikhare2@gmail.com>
2025-02-06 08:33:57 +0530

    FSD

--- javascript/index31.js
+++ javascript/index31.js
@@ -1,2 +1,36 @@
 // everything in a js is a object 
 // dot property is used whrn there is a object
+
+// let obj={
+//     id:1,
+//     name:"hello"
+// }
+// console.log(obj.__proto_._proto_,"hehehe");
+// console.log(obj.map(),"hehehe");
+
+// console.log(obj.toString());
+// console.log(obj.id);
+// console.log(obj.fullname);
+
+// let arr=[1,2,3,4,5]
+// arr.map(()=>{
+
+// })
+// console.log(arr.__proto__.__proto__,"dekho");
+
+Array.prototype.myMap=function(callback){
+
+    let newArr=[]
+    for(let i=0;i<this.length;i++){
+        newArr.push(callback(this[i],i,this))
+    }
+    return newArr
+}
+
+let arr=[1,2,3,4,5]
+let data=arr.myMap((a,b,c)=>{
+    return a+2
+})
+console.log(data,"datata");
+
+
