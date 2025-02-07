db5f15d1b046049b46b2d0cd1bf6a099241bf38a
palkikhare21 <palkikhare2@gmail.com>
2025-02-07 07:49:05 +0530

    FSD

--- javascript/index31.js
+++ javascript/index31.js
@@ -18,19 +18,56 @@
 // })
 // console.log(arr.__proto__.__proto__,"dekho");
 
-Array.prototype.myMap=function(callback){
+// Array.prototype.myMap=function(callback){
 
-    let newArr=[]
-    for(let i=0;i<this.length;i++){
-        newArr.push(callback(this[i],i,this))
-    }
-    return newArr
-}
+//     let newArr=[]
+//     for(let i=0;i<this.length;i++){
+//         newArr.push(callback(this[i],i,this))
+//     }
+//     return newArr
+// }
 
-let arr=[1,2,3,4,5]
-let data=arr.myMap((a,b,c)=>{
-    return a+2
-})
-console.log(data,"datata");
+// let arr=[1,2,3,4,5]
+// let data=arr.myMap((a,b,c)=>{
+//     return a+2
+// })
+// console.log(data,"datata");
+
+
+// Array.prototype.myFilter=function(callback){
+//     let newArr=[]
+//     for(let i=0;i<this.length;i++){
+//         if(callback(this[i],i,this)){
+//             newArr.push(this[i])
+//         }
+//     }
+//     return newArr
+// }
+// let arr=[1,2,3,45,6,67]
+// let data=arr.myFilter((a)=>{
+//     return a>2
+// })
+// console.log(data);
+
+const promise = new Promise(function C(resolve, reject) {
+    console.log(1);
+    setTimeout(function D() {
+        console.log("timerStart");
+        resolve("success"); 
+        console.log("timerEnd");  
+    }, 0);
+    console.log(2);
+});
+
+promise.then(function f(res) {
+    console.log(res);
+});
+
+setTimeout(function E() {
+    console.log("Are!!");
+}, 0);
+
+console.log(4);
+
 
 
