97f6ec1d047426b28b6078a2762326dff40c345a
palkikhare21 <palkikhare2@gmail.com>
2025-03-05 20:32:02 +0530

    FSD

--- yt/index.js
+++ yt/index.js
@@ -1,8 +1,26 @@
-var x =7;
+// var x =7;
 
-function getName() {
-    console.log("namaste javascript");
+// function getName() {
+//     console.log("namaste javascript");
     
-}
-getName();
-console.log(x);
+// }
+// getName();
+// console.log(x);
+
+
+// variable  hoisting
+
+// {
+//    var x=1;
+    
+// }
+// console.log(x);
+
+// console.log(age);
+
+// var age=25;
+// refernce error
+// console.log(age);
+// let age=25;
+
+
