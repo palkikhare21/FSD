3d8741126c90809bce4d468ca30c9fb327743d71
palkikhare21 <palkikhare2@gmail.com>
2025-04-05 15:17:50 +0530

    FSD

--- JS/index.js
+++ JS/index.js
@@ -1,1 +1,26 @@
-console.log("apna collage");
+// console.log("apna collage");
+// var age=24;  //variables
+// console.log(age);
+
+// let fullName='palkikhare';
+// console.log(fullName);
+
+// const PI=4.35;
+// console.log(PI);
+
+// const Product={
+//     name:'pen',
+//     rating:5,
+//     price:250,
+
+// }
+// console.log(Product);
+
+const Profile={
+    name:'palki khare',
+    isfollow:true,
+    followers:900,
+    following:100
+
+}
+console.log(typeof(Profile));
