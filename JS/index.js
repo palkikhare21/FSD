68570d6b2d21ba19b37662376a977d56aafec741
palkikhare21 <palkikhare2@gmail.com>
2025-04-06 15:20:44 +0530

    FSD

--- JS/index.js
+++ JS/index.js
@@ -16,11 +16,61 @@
 // }
 // console.log(Product);
 
-const Profile={
-    name:'palki khare',
-    isfollow:true,
-    followers:900,
-    following:100
+// const Profile={
+//     name:'palki khare',
+//     isfollow:true,
+//     followers:900,
+//     following:100
 
+// }
+// console.log(typeof(Profile));
+
+// let a=5
+// let b=2
+
+// console.log(a**b,"=");
+
+//IFFFFFFFFFF
+
+// let age = 16;
+//  if (age==25){
+//     console.log('vote');
+    
+//  }
+//  if (age<=18){
+//     console.log('not vote');
+    
+//  }
+
+// IF ELSEEEEEEEE
+
+// let mode='light';
+// let color;
+// if (mode==='dark'){
+//     color="black";
+// }else{
+//     color="white";
+// }
+// console.log(color);
+
+//ODD EVEN
+// let num =27;
+// if(num%2===0){
+//     console.log('even');
+    
+// }
+// else{
+//     console.log('odd');
+    
+// else if statment
+let mode='pink';
+let color;
+if ( mode==='dark'){
+    color='black';
+}else if (mode==='pink'){
+    color='pink';
+}else{
+    color='white';
 }
-console.log(typeof(Profile));
+console.log(color);
+
