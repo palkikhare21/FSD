a1ab08ef1eb3370ca7c296bbb9205b1e117b6d00
palkikhare21 <palkikhare2@gmail.com>
2025-04-10 14:31:26 +0530

    FSD

--- JS/practice.js
+++ JS/practice.js
@@ -37,3 +37,14 @@
 //     }
 // }
 //2
+let marks=[85,97,44,37,76,60]
+let sum =0;
+
+for(let val of marks){
+    sum+=val;
+}
+console.log(sum);
+let avg= sum / marks.length;
+console.log(`avg marks of the class=${avg}`);
+
+
