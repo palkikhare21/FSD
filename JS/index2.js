b1eb615f8682db3c5e1d00e9640db311b741f490
palkikhare21 <palkikhare2@gmail.com>
2025-05-08 22:26:20 +0530

    FSD

---
+++ JS/index2.js
@@ -0,0 +1,12 @@
+let btn1 = document.querySelector("#btn1");
+
+btn1.onclick = ()=>{
+    console.log("this was clicked");
+    
+}
+
+let div = document.querySelector("div");
+div.onmouseover=()=>{
+    console.log("you r inside the box");
+    
+}
