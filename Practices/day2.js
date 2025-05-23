b51de992f27b2e5335d411139527f5f6a75b8b46
palkikhare21 <palkikhare2@gmail.com>
2025-05-23 21:53:18 +0530

    FSD

--- Practices/day2.js
+++ Practices/day2.js
@@ -1,5 +1,5 @@
 
-let arr=[1,2,3,4,5,6,7,8]
+// let arr=[1,2,3,4,5,6,7,8]
 
 
 // for(let i of arr){
@@ -12,9 +12,94 @@ let arr=[1,2,3,4,5,6,7,8]
     
 // })
 
-let data1 = arr.map((a,b,c)=>{
-    console.log(a);
+// let data1 = arr.map((a,b,c)=>{
+//     console.log(a);
     
-})
+// })
 
-console.log(data1==arr); //map creates a new arr so the address are not same
+// console.log(data1==arr); //map creates a new arr so the address are not same
+
+// let data = arr.filter((a,b,c)=>{
+//     return a>3
+    
+// })
+// console.log(data);
+
+// let data1 = arr.reduce((a,b,c)=>{
+//     return a>b?a:b
+// })
+// console.log(data1);
+
+// let arr = [2,3,2,3,42,4,32,5,2,5,7]
+
+// let data = arr.filter((a,b,c)=>{
+//     return a>2
+// })
+//  let data1=data.filter((a)=>{
+//     return a%2==0
+// })
+// let sum = arr.reduce((a,b,c,d)=>{
+//     return a+b
+    
+// })
+// console.log(data);
+// console.log(data1);
+
+ 
+// console.log(sum);
+
+// let arr = [0,20,30]
+
+// function convertTemps(array){
+//     return arr*(9/5)+32
+// }
+// console.log();
+//2nd max value
+//another array in dono ko compare n which has lagerest value
+let arr=[1,2,3,4,5,6,7,8]
+// console.log(arr.includes(3));
+// console.log(arr.indexOf(100));
+
+
+// let data=arr.find((a,b,c)=>{
+//     return a==3
+// })
+// console.log(data);
+// let data1=arr.filter((a,b,c)=>{
+//     return a==3
+// })
+// let maxValue = arr.reduce((a,b,c)=>{
+//     return a>b?a:b
+// })
+// let Newarr = arr.filter((a,b,c)=>{
+//     return a!==maxValue
+// })
+// let secondMax = Newarr.reduce((a,b,c)=>{
+//     return a>b?a:b
+// })
+
+// console.log(maxValue);
+// console.log(Newarr);
+// console.log(secondMax);
+let arr1=[1,2]
+let merge=[]
+for(let i of arr){
+    merge.push(i)
+   
+}
+ console.log(merge);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
